b993b4733ca5c6122c6385656edcc24c002a3798
Ananya Singh <ananyass765@gmail.com>
2024-09-07 01:30:16 +0530

    first commit

--- ad.js
+++ ad.js
@@ -2,5 +2,20 @@ Shery.mouseFollower()
 Shery.makeMagnet(".magnet-target") ; 
  var nav=document.querySelector("#nav");
 
+ Shery.imageMasker(".mask-target" /* Element to target.*/, {
+    //Parameters are optional.
+    mouseFollower: true,
+    text: "Shery",
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
 
-    
+//     Shery.textAnimate(".text-target" /* Element to target.*/, {
+//   //Parameters are optional.
+//   style: 1,
+//   y: 10,
+//   delay: 0.1,
+//   duration: 2,
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   multiplier: 0.1,
+// });
